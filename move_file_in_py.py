6346cf900590420e4e93833fe42fb66385e5de8e
MantisA-Z <160739281+MantisA-Z@users.noreply.github.com>
2024-02-23 03:34:04 +0530

    move_file_in_python
    This code is about moving files form one place to another using python, using the os.replace() function in python.

---
+++ move_file_in_py.py
@@ -0,0 +1,19 @@
+import os
+
+#variable (source) and (destinantion) not necessary you can directly change the src and dest while calling the function in the last line
+source = 'test.txt'
+destination = 'C:\\Users\\MANTIS\\OneDrive\\new'
+
+#main function
+def move_file(source, destination):
+    try:
+        if not os.path.exists(destination):
+            os.replace(source, destination)
+            print(source +'File was moved to ' + destination)
+        else:
+            print(f'There is already a file in {destination}')
+    except FileNotFoundError:
+        print(f'There is no file {source}')
+
+#Executing code
+move_file(source, destination)
