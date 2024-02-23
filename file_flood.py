ced4a4aeaebcb5b154df93f0664063493ec8cc4f
MantisA-Z <160739281+MantisA-Z@users.noreply.github.com>
2024-02-23 12:54:34 +0530

    Update file_flood.py

--- file_flood.py
+++ file_flood.py
@@ -1,6 +1,6 @@
 i = 0
-text = f'Hello World!\n' * 5
-while i <= 0:
+text = f'Hello World!\n' * 500
+while i <= 1000:
     with open(f'file.txt{i}', 'w') as file:
         file.write(text)
     i += 1
